73afc4a290a04c50e47b0296bd5965ef16042b32
Garcia <gsrijnders@hotmail.com>
2017-03-24 11:57:11 +0100

    asdad

---
+++ app/model/Customer.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Murdock on 10/9/2016.
+ */
+public class Customer extends User
+{
+  public Customer(String username, String password)
+    {
+        super(username, password);
+
+     }
+
+
+
+    @Override
+    public String toString()
+    {
+        return super.toString();
+    }
+}
---
+++ app/model/Owner.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Murdock on 10/9/2016.
+ */
+public class Owner extends User
+{
+
+    public Owner(String username, String password)
+    {
+        super(username, password);
+
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return super.toString();
+    }
+}
